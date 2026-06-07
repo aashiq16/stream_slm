068a1c521c2ed6f4d296e112a394dffde21dd220
aashiq16 <87873115+aashiq16@users.noreply.github.com>
2026-06-07 13:44:16 +0530

    Update slm_service.py

--- services/slm_service.py
+++ services/slm_service.py
@@ -2,24 +2,13 @@ import requests
 import streamlit as st
 
 API_KEY = st.secrets["OLLAMA_API_KEY"]
-API_ENDPOINT = st.secrets["API_ENDPOINT"]
 
-def ask_slm(prompt):
+response = requests.get(
+    "https://ollama.com/v1/models",
+    headers={
+        "Authorization": f"Bearer {API_KEY}"
+    }
+)
 
-    response = requests.post(
-        API_ENDPOINT,
-        headers={
-            "Authorization": f"Bearer {API_KEY}",
-            "Content-Type": "application/json"
-        },
-        json={
-            "model": "llama3.2:3b",
-            "prompt": prompt,
-            "stream": False
-        },
-        timeout=120
-    )
-
-    response.raise_for_status()
-
-    return response.json()["response"]
+st.write("Status:", response.status_code)
+st.write(response.text)
