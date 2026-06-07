7eb2849c74016194482ccdffd684ce6b6337f527
aashiq16 <87873115+aashiq16@users.noreply.github.com>
2026-06-07 13:47:52 +0530

    Update slm_service.py

--- services/slm_service.py
+++ services/slm_service.py
@@ -3,12 +3,32 @@ import streamlit as st
 
 API_KEY = st.secrets["OLLAMA_API_KEY"]
 
-response = requests.get(
-    "https://ollama.com/v1/models",
-    headers={
-        "Authorization": f"Bearer {API_KEY}"
-    }
-)
+MODEL_NAME = "gemma3:4b"
 
-st.write("Status:", response.status_code)
-st.write(response.text)
+
+def ask_slm(prompt):
+
+    response = requests.post(
+        "https://ollama.com/api/chat",
+        headers={
+            "Authorization": f"Bearer {API_KEY}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "model": MODEL_NAME,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            "stream": False
+        },
+        timeout=120
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    return data["message"]["content"]
